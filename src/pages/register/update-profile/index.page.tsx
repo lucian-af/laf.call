b0d8148df912b1fdef53cd8a952a8388aa7e8697
Lucian AF <lucian_af@hotmail.com>
2024-03-06 20:07:59 -0300

    fix(update-profile): corrige parâmetro na rota de schedule.

--- src/pages/register/update-profile/index.page.tsx
+++ src/pages/register/update-profile/index.page.tsx
@@ -43,7 +43,7 @@ export default function UpdateProfile() {
       bio: data.bio,
     })
 
-    await router.push(`/schedule/${session.data?.user.name}`)
+    await router.push(`/schedule/${session.data?.user.username}`)
   }
 
   return (
